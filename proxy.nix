80ca8645e429c94e5d061ce13584bba936bc71d2
GTrunsec <gtrunsec@hardenedlinux.org>
2020-04-05 23:10:53 -0700

    reset

--- proxy.nix
+++ proxy.nix
@@ -1,6 +1,6 @@
 { ... }:
 let
-  pkgs = (import <nixpkgs> { config.allowUnfree = true; config.proxy = "http://127.0.0.1:8123";});
+  pkgs = (import <nixpkgs> { config.allowUnfree = true;});
 
 in {
 
